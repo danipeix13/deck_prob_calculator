eccbaee34b0c4113b8f28ef829ea398f6a3063ae
danipeix13 <dpeixdel@alumnos.unex.es>
2022-06-02 04:52:27 +0200

    Logic implemented

--- calculator.py
+++ calculator.py
@@ -1,6 +1,22 @@
+import numpy as np
+
+X = "WHATEVER"
 
 
 class Calculator():
-    def __init__(self):
-        pass
+    def __init__(self, deck_and_probs):
+        self.deck, self.probs = list(zip(*deck_and_probs))
+        
+    def calculateProbs(self, combo_hands, num_iters=10):
+        dic = { "A": 0, "B": 0, "C": 0, "D": 0, "E": 0 }
+        cont = 0
+        for i in range(num_iters):
+            hand = set(np.random.choice(self.deck, 5, True, np.array(self.probs) / np.sum(self.probs)))
+            for ch in combo_hands:
+                if len(hand.intersection(ch)) == len(ch):
+                    cont += 1
+                    break
+        print(cont * 100 / num_iters, "%", dic)
+
+
     
--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-import numpy as np
-from calculator import *
+from calculator import Calculator
 
 deck_and_probs = [
-    ["P",  9], # Piezas y reactivaciones
-    ["H",  3], # Hangar
-    ["B",  7], # Brave
-    ["HS", 9], # Handtraps
-    ["T",  6], # Therion
-    ["O",  4], # Brick
-    ["TF", 1], # Terraforming
-    ["SR", 1], # Set Rotation
+    ["A", 1], 
+    ["B", 4],  
 ]
 
-X = "WHATEVER"
-combo_hands = set(
-    set(),
-    set(),
-    set(),
-    set(),
-    set(),
-    set(),
-    set(),
-    set(),
-    set(),
+combo_hands = [
+    {"A"},
+]
 
-)
+if __name__ == "__main__":
+    print(type(deck_and_probs), type(combo_hands))
+    c = Calculator(deck_and_probs)
+    c.calculateProbs(combo_hands, 100000)
 
-deck, probs = [], []
-for k,v in deck_and_probs.items():
-
-
-np.random.choice(deck, 5, probs)
