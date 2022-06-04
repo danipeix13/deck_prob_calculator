412c1e5ebe6a3274b90cf11cfa7c013381a9bd42
danipeix13 <dpeixdel@alumnos.unex.es>
2022-06-04 12:28:34 +0200

    SCRIPT VERSION DONE

--- calculator.py
+++ calculator.py
@@ -1,31 +1,83 @@
-from nis import match
+from itertools import combinations
+from collections import Counter
+import matplotlib.pyplot as plt
 import numpy as np
 
 X = "WHATEVER"
 
 
 class Calculator():
-    def __init__(self, deck_and_probs):
-        self.deck, self.probs = list(zip(*deck_and_probs))
+    def __init__(self, probs_and_cards, hand_size, combo_hands):
+        self.hand_size = hand_size
+
+        self.possible_hands = self.__allHands(probs_and_cards)
+        #print("HANDS:", len(self.possible_hands), "\n", self.possible_hands)
+
+        self.combo_hands = self.__addComboHands(combo_hands)
+        self.combo_hands_list = combo_hands 
+        #print("COMBO HANDS", len(self.combo_hands), "\n", self.combo_hands)
+
+        self.results = {}
+
+    def calculateProbs(self):
+        self.matches = np.zeros(len(self.combo_hands))
+        self.combo = 0
+        for h in self.possible_hands:
+            match = False
+            for i in range(len(self.combo_hands)):
+                if self.__match(h, self.combo_hands[i]):
+                    self.matches[i] += 1
+                    match = True
+            if match:
+                self.combo += 1
+        # print(self.matches, self.combo)
+        self.results = {
+            "TOTAL_COMBO": self.__format(self.combo * 100 / len(self.possible_hands)),
+            "HAND_VALUES": list(self.matches * 100 / len(self.possible_hands)),
+            "HAND_NAMES" : self.combo_hands_list,
+            "N_VALUES"   : np.arange(len(self.matches)),
+        }
+
+    
+        self.results = {
+            "TOTAL_COMBO": self.__format(self.combo * 100 / len(self.possible_hands)),
+            "HAND_VALUES": [x for x in list(self.matches * 100 / len(self.possible_hands))],
+            "HAND_NAMES" : self.combo_hands_list,
+            "N_VALUES"   : np.arange(len(self.matches)),
+        }
+ 
+    def printResults(self):
+        print("COMBO HANDS: {}%".format(self.results["TOTAL_COMBO"]))
+        for name,value in list(zip(self.results["HAND_NAMES"], self.results["HAND_VALUES"])):
+            print("{}: {}%".format(name, self.__format(value)))
+
+    def plotResults(self):
+        print(self.results["N_VALUES"])
+        for x,y in list(zip(self.results["N_VALUES"], self.results["HAND_VALUES"])):
+            plt.annotate(str(self.__format(y)), (x,y))
+        plt.xticks(self.results["N_VALUES"], self.results["HAND_NAMES"])
+        plt.yticks(np.arange(0, 101, 5))
+        plt.title("COMBO HANDS: {}%".format(self.results["TOTAL_COMBO"]))
+        plt.plot(self.results["N_VALUES"], self.results["HAND_VALUES"], "bo")
+        plt.bar(self.results["N_VALUES"], self.results["HAND_VALUES"])
+        plt.show()
+
+    ###############################################################################
+    def __format(self, data):
+        f = "{:.2f}"
+        return f.format(data)
         
-    def calculateProbs(self, combo_hands, num_iters=10):
-        matches, cont = [], 0
-        for ch in combo_hands.values():
-            matches.append(0)
+    def __allHands(self, probs_and_cards):
+        deck = []
+        for p_c in probs_and_cards:
+            deck += [p_c[1] for _ in range(p_c[0])]
+        deck = list(combinations(deck, self.hand_size))
+        return [Counter(d) for d in deck]
+
+    def __addComboHands(self, combo_hands):
+        return [Counter(ch) for ch in combo_hands]
+
+    def __match(self, h1, h2):
+        return sum(h1.values()) >= sum(h2.values()) and h1 & h2 == h2
             
-        for _ in range(num_iters):
-            hand = set(np.random.choice(self.deck, 5, True, np.array(self.probs) / np.sum(self.probs)))
-            isMatch = False
-            for id,ch in combo_hands.items():
-                if len(hand.intersection(ch)) == len(ch):
-                    isMatch = True
-                    matches[id] += 1
-            if isMatch:
-                cont += 1
-
-        print("TOTAL COMBO HANDS:", cont * 100 / num_iters, "%")
-        for id,ch in combo_hands.items():
-            print(ch, ":", matches[id] * 100 / num_iters, "%")
-
-
     
--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 from calculator import *
 
-deck_and_probs = [
-    ["Brave", 7],
-    ["Therions", 6],
-    ["ABC", 7],
-    ["Hangar", 3],
-    ["Terra", 1],
-    ["SetRot", 1],
-    ["Second", 9],
-    ["React", 2],
-    [X, 4],
+DECK = [
+    [15, "Starter"],
+    [12, "Extender"],
+    [8, "Handtrap"],
+    [5, "BRICK"],
 ]
 
-combo_hands = {
-    0: {"ABC"},
-    1: {"Brave"},
-    2: {"ABC", "Brave"},
-    # 0: {"ABC", "Brave", "Therions"},
-    # 1: {"Terra", "Brave", "Therions"},
-    # 2: {"ABC", "Brave", "Terra"},
-    # 3: {"SetRot", "Brave"},
-}
+COMBO_HANDS = [
+    ["Starter", "Starter"],
+    ["Starter", "Handtrap"],
+    ["Starter", "Extender", "Extender"],
+    ["Extender", "Extender", "Handtrap"],
+    ["BRICK", "BRICK"],
+]
+
+HAND_SIZE = 5
 
 if __name__ == "__main__":
-    c = Calculator(deck_and_probs)
-    c.calculateProbs(combo_hands, 100000)
-
+    c = Calculator(DECK, HAND_SIZE, COMBO_HANDS)
+    c.calculateProbs()
+    c.printResults()
+    c.plotResults()
